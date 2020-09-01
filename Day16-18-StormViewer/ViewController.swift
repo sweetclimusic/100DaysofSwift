5bf6a265da2fc98c87390a7ab8c6fa8595684e90
Ashlee Muscroft <ashlee.muscroft@fathom3.com>
2020-09-01 21:16:35 +0100

    Initial Commit

---
+++ Day16-18-StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day16-18-StormViewer
+//
+//  Created by Ashlee Muscroft on 01/09/2020.
+//  Copyright © 2020 Ashlee Muscroft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
