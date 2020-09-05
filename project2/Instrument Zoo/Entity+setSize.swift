b9aad6581b515451c0a97c9476bfe6722f6cb2e0
Ashlee Muscroft <ashlee.muscroft@fathom3.com>
2020-09-06 00:36:08 +0100

    changes to instrument zoo

--- project2/Instrument Zoo/Entity+setSize.swift
+++ project2/Instrument Zoo/Entity+setSize.swift
@@ -12,6 +12,6 @@ extension Entity {
     func setSize(_ size: Float, relativeTo entity: Entity?) {
         let currentSize = self.visualBounds(recursive: true, relativeTo: entity, excludeInactive: false).extents.max()
         let scaleFactor = size / currentSize
-        setScale([scaleFactor, scaleFactor, scaleFactor], relativeTo: nil)
+        setScale([scaleFactor, scaleFactor, scaleFactor], relativeTo: entity)
     }
 }
