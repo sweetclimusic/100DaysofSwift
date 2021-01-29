e80545a0486ea8a2beda5c51ee9a2a1cfcf176cc
Ashlee Muscroft <ashlee.muscroft@fathom3.com>
2021-01-29 02:19:44 +0000

    Squashed 'project12/' content from commit c26d471
    git-subtree-dir: project12
    git-subtree-split: c26d471c41964d1c8e9d673ea0fa0ab711a911f3

---
+++ Minnow WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Minnow WatchKit Extension
+//
+//  Created by Ashlee Muscroft on 20/08/2020.
+//  Copyright © 2020 Ashlee Muscroft. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
